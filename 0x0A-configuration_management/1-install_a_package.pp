b821167441d513b74f80472ece7eb281a1029295
Georgen21 <chukwudinwabia42@gmail.com>
2024-01-20 04:28:26 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Using Puppet, install flask from pip3.
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip',
+  require => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure => '2.1.1',
+  provider => 'pip',
+  require => Package['python3-pip'],
 }
