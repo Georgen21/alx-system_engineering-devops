51b14067929bc22293f4b199c20964b6137c520e
Georgen21 <chukwudinwabia42@gmail.com>
2024-03-05 13:53:09 -0800

    How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+contains queries to https://www.reddit.com/dev/api/
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers
+    """
+    domain = 'https://www.reddit.com'
+    path = '/r/{}/about.json'.format(subreddit)
+    url = '{}{}'.format(domain, path)
+    header = {
+        'user-agent': '0x16-api_advanced:project/1.0 (by /u/chukwudinwabia42)'
+    }
+    response = requests.get(
+        url,
+        headers=header,
+        allow_redirects=False
+    )
+    code = response.status_code
+    if code >= 300:
+        return 0
+    data = response.json().get('data')
+    subscribers = data.get('subscribers')
+    return subscribers
